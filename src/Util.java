7a42395fb99a71e21201c4a3d19b881aa59ecedd
king-sunshine-spring <1348066599@qq.com>
2018-01-18 17:23:46 +0800

    @wgcwgc TtsServer
    @wgcwgc TtsServer
    add server interface
    @ author  wgcwgc
    @ time 2017.01.18
    @ address runcom
    @ config intel(R) Core (TM) i5 - 2310 cpu @ 2.90GHz 3.20GHz
    8.00GB(7.89GB可用) 64bit
    windows 7 旗舰版
    @ detail mark

---
+++ src/Util.java
@@ -0,0 +1,48 @@
+import java.io.File;
+import java.io.IOException;
+
+/**
+ * 
+ */
+
+/**
+ * @author           Administrator
+ * @copyright        wgcwgc
+ * @date             2018年1月3日
+ * @time             下午2:06:26
+ * @project_name     TtsServer
+ * @package_name     
+ * @file_name        Util.java
+ * @type_name        Util
+ * @enclosing_type   
+ * @tags             
+ * @todo             
+ * @others           
+ *
+ */
+
+public class Util
+{
+	public static String rootPath = null;
+	public static String getPath()
+	{
+		try
+		{
+			rootPath = new File(".").getCanonicalPath().toString() + "\\";
+		}
+		catch(IOException e)
+		{
+			rootPath = System.getProperty("user.dir") + "\\";
+		}
+		return rootPath;
+	}
+	
+	/**
+	 * @param args
+	 */
+	public static void main(String [] args)
+	{
+		
+	}
+	
+}
